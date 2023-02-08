6ed2c4b7d8d7f485e77d7b8efc700902b73de794
daniel gutierrez <daniel_gutierrezn15@hotmail.com>
2023-02-07 22:41:19 -0300

    first commit

---
+++ db/productos.js
@@ -0,0 +1,4 @@
+const Notebook = { title: "Notebook", description: "Notebook intel celeron", price: 100100, thumbnail: "https://http2.mlstatic.com/D_NQ_NP_926977-MLA51903807543_102022-O.webp", code: 1, stock: 8 };
+const zapatilla = { title: "Zapatilla", description: "Zapatilla para Running", price: 12000, thumbnail: "https://http2.mlstatic.com/D_NQ_NP_2X_618071-MLA49444510078_032022-F.webp", code: 1, stock: 29 };
+
+module.exports = { Notebook, zapatilla };
